f8c75941466a339a1fb2c4cb2affe7ad9fb452af
cylynex <sam@westoforange.com>
2019-12-17 11:09:03 -0500

    DB connection setup and includes

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Exercise Buddy v1</title>
+	<meta charset="UTF-8">
+	<title>Exercise Buddy v1</title>
+	
+	<?php // Includes TODO move to separate file
+	include("includes/database.php");
+	?>
+	
+	
 </head>
 
 <body>
