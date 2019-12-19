c5f0eb54531759c53cd1e51d1e00b89bf1cceeec
cylynex <root@westoforange.com>
2019-12-19 14:25:24 -0500

    Routines screens infrastructure

--- classes/Routines.php
+++ classes/Routines.php
@@ -6,8 +6,19 @@ class Routines extends Database {
 		echo "universal routines";
 	}
 	
-	public function ShowUniversalRoutines() {
-		echo "here";
+	// Get Routines Matching user ID
+	// 0 for universal
+	public function GetRoutines($id) {
+		$routs = $this->Query("SELECT * FROM routines WHERE routineUserID = '$id' ");
+		echo "Routines Found: ".mysqli_num_rows($routs);
+		return $routs;
+	}
+	
+	// Show list of Routines
+	public function ShowRoutineList($routineList) {
+		foreach($routineList as $thisRoutine) {
+		?><p><a href="/Routines/View/<?php echo $thisRoutine['routineID']?>"><?php echo $thisRoutine['routineName']?></a></p><?php
+		}
 	}
 	
 }
--- pages/routines.php
+++ pages/routines.php
@@ -1,16 +1,26 @@
-<strong>Routines</strong><br>
+<p><strong>Routines</strong> (<a href="/Routines">Back</a>)</p>
 
 <?php
 $urls = $rewrite->GetUrlVariables();
 
-if ($url2 == "Universal") { $routines->ShowUniversalRoutines(); }
+// Show universal Routines
+if ($url2 == "Universal") { 
+	$routs = $routines->GetRoutines(0); 
+	$routines->ShowRoutineList($routs);
+}
+
+// Show Routine Details
+if ($url2 == "View") {
+	echo "show the routine here.";
+}
+
 else { ShowMenu(); }
 
 
 function ShowMenu() {
 	?>
 	<p><a href="/Routines/Universal">View Universal Routines</a></p>
-	View My Routines
-	Create a New Routine
+	<p>View My Routines</p>
+	<p>Create a New Routine</p>
 	<?php
 }
